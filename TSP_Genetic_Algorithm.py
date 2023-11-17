c27a4d370e5cc4fea8e062e2871e40347c07737b
SonnyFixit <fikser@tlen.pl>
2023-11-17 15:57:00 +0100

    Further improvement for mutation functions
    - 7500-8500 in around 10-17 sec. High time improvement, small solution drop
    - This change may be reverted

--- TSP_Genetic_Algorithm.py
+++ TSP_Genetic_Algorithm.py
@@ -1,5 +1,6 @@
 import random
-import concurrent.futures
+""" import cProfile
+from line_profiler import LineProfiler """
 
 def load_triangular_matrix(file_path):
     with open(file_path, 'r') as file:
@@ -46,9 +47,10 @@ def initialize_population(pop_size, num_cities):
         population.append(route)
     return population
 
-def tournament_selection(population, distances, k):
+
+def tournament_selection(population, distances, k, fitness_values):
     selected = random.sample(population, k)
-    return min(selected, key=lambda x: total_distance(x, distances))
+    return min(selected, key=lambda x: fitness_values[population.index(x)])
 
 def pmx_crossover(parent1, parent2):
     size = len(parent1)
@@ -56,38 +58,47 @@ def pmx_crossover(parent1, parent2):
     if a > b:
         a, b = b, a
 
-    child = parent1[a:b+1]
-    child_set = set(child)
+    child = [None] * size
+    child_set = set()
 
+    # Copy the selected portion from parent1
+    child[a:b+1] = parent1[a:b+1]
+    child_set.update(child[a:b+1])
+
+    # Map genes in the selected portion from parent2 to their indices
+    gene_to_index = {gene: idx for idx, gene in enumerate(parent2)}
+
+    # Fill the rest of the child using parent2
     for i in range(size):
         if i < a or i > b:
             gene = parent2[i]
             while gene in child_set:
-                idx = parent2.index(gene)
+                idx = gene_to_index[gene]
                 gene = parent2[(idx + 1) % size]
-            child.append(gene)
+            child[i] = gene
             child_set.add(gene)
 
     return child
 
+
 def inversion_mutation(route):
-    a, b = random.sample(range(len(route)), 2)
-    if a > b:
-        a, b = b, a
+    a, b = sorted(random.sample(range(len(route)), 2))
     route[a:b+1] = reversed(route[a:b+1])
     return route
+
 
 def exchange_mutation(route):
     a, b = random.sample(range(len(route)), 2)
     route[a], route[b] = route[b], route[a]
     return route
 
-def generate_population_and_evaluate(population, distance_lookup, tournament_size):
+
+def generate_population_and_evaluate(population, distance_lookup, tournament_size, fitness_values):
     new_population = []
 
     for _ in range(len(population) // 2):
-        parent1 = tournament_selection(population, distance_lookup, tournament_size)
-        parent2 = tournament_selection(population, distance_lookup, tournament_size)
+        parent1 = tournament_selection(population, distance_lookup, tournament_size, fitness_values)
+        parent2 = tournament_selection(population, distance_lookup, tournament_size, fitness_values)
 
         if random.random() < crossover_prob:
             child1 = pmx_crossover(parent1, parent2)
@@ -120,7 +131,8 @@ def genetic_algorithm(distance_matrix, pop_size, tournament_size, crossover_prob
     population = initialize_population(pop_size, len(distance_matrix))
     
     for generation in range(num_generations):
-        new_population = generate_population_and_evaluate(population, distance_lookup, tournament_size)
+        fitness_values = [total_distance(individual, distance_lookup) for individual in population]
+        new_population = generate_population_and_evaluate(population, distance_lookup, tournament_size, fitness_values)
         population = new_population
 
     best_route = min(population, key=lambda x: total_distance(x, distance_lookup))
@@ -139,12 +151,40 @@ distance_lookup = create_distance_lookup(symmetric_matrix)
 pop_size = 1500
 tournament_size = 10
 crossover_prob = 0.8
-inversion_prob = 0.1
-exchange_prob = 0.1
-num_generations = 150
+inversion_prob = 0.15
+exchange_prob = 0.15
+num_generations = 250
+
+""" # Profiling with cProfile
+cprofiler = cProfile.Profile()
+cprofiler.enable() """
 
 best_route, best_distance = genetic_algorithm(symmetric_matrix, pop_size, tournament_size, crossover_prob, inversion_prob, exchange_prob, num_generations, distance_lookup=distance_lookup)
 
 print("\nBest Route:")
 print(best_route + [best_route[0]])
 print("\nBest Distance:", best_distance)
+
+""" cprofiler.disable()
+cprofiler.print_stats(sort='cumulative')
+
+# Profiling with line_profiler
+profiler = LineProfiler()
+
+# Add functions to be profiled
+profiler.add_function(total_distance)
+profiler.add_function(initialize_population)
+profiler.add_function(tournament_selection)
+profiler.add_function(pmx_crossover)
+profiler.add_function(inversion_mutation)
+profiler.add_function(create_distance_lookup)
+profiler.add_function(exchange_mutation)
+profiler.add_function(genetic_algorithm)
+profiler.add_function(generate_population_and_evaluate)
+
+# Run the code while profiling
+profiler_wrapper = profiler(genetic_algorithm)
+best_route, best_distance = genetic_algorithm(symmetric_matrix, pop_size, tournament_size, crossover_prob, inversion_prob, exchange_prob, num_generations, distance_lookup=distance_lookup)
+
+# Print the results
+profiler.print_stats() """
