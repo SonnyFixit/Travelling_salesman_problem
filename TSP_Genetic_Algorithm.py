3ac158e094eed1485d678999779980da3fa6ac6d
SonnyFixit <fikser@tlen.pl>
2023-11-17 15:24:44 +0100

    Performance improved - storing calculated distance
    - Now distance is being stored, 7500-8500 in 23 sec

--- TSP_Genetic_Algorithm.py
+++ TSP_Genetic_Algorithm.py
@@ -1,7 +1,5 @@
-import numpy as np
 import random
 import concurrent.futures
-from numba import njit
 
 def load_triangular_matrix(file_path):
     with open(file_path, 'r') as file:
@@ -20,12 +18,25 @@ def make_symmetric(matrix):
 
     return symmetric_matrix
 
-def total_distance(route, distance_matrix):
-    total_dist = 0.0
-    for i in range(len(route)):
-        total_dist += distance_matrix[route[i], route[(i + 1) % len(route)]]
-    return total_dist
+def create_distance_lookup(distance_matrix):
+    num_cities = len(distance_matrix)
+    distance_lookup = {}
 
+    for i in range(num_cities):
+        for j in range(num_cities):
+            distance_lookup[(i, j)] = distance_matrix[i][j]
+
+    return distance_lookup
+
+def total_distance(route, distance_lookup):
+    total_dist = 0.0
+    num_cities = len(route)
+
+    for i in range(num_cities):
+        # Look up the distance in the precalculated table
+        total_dist += distance_lookup[(route[i], route[(i + 1) % num_cities])]
+
+    return total_dist
 
 def initialize_population(pop_size, num_cities):
     population = []
@@ -45,11 +56,9 @@ def pmx_crossover(parent1, parent2):
     if a > b:
         a, b = b, a
 
-    # Copy the selected part from parent1 to the child
     child = parent1[a:b+1]
     child_set = set(child)
 
-    # Fill the remaining elements from parent2
     for i in range(size):
         if i < a or i > b:
             gene = parent2[i]
@@ -68,100 +77,63 @@ def inversion_mutation(route):
     route[a:b+1] = reversed(route[a:b+1])
     return route
 
-def evaluate_route(route, distance_matrix):
-    return total_distance(route, distance_matrix)
-
 def exchange_mutation(route):
     a, b = random.sample(range(len(route)), 2)
     route[a], route[b] = route[b], route[a]
     return route
 
-def genetic_algorithm(distance_matrix, pop_size, tournament_size, crossover_prob, inversion_prob, exchange_prob, num_generations):
+def generate_population_and_evaluate(population, distance_lookup, tournament_size):
+    new_population = []
+
+    for _ in range(len(population) // 2):
+        parent1 = tournament_selection(population, distance_lookup, tournament_size)
+        parent2 = tournament_selection(population, distance_lookup, tournament_size)
+
+        if random.random() < crossover_prob:
+            child1 = pmx_crossover(parent1, parent2)
+            child2 = pmx_crossover(parent2, parent1)
+        else:
+            child1, child2 = parent1[:], parent2[:]
+
+        if random.random() < inversion_prob:
+            child1 = inversion_mutation(child1)
+        if random.random() < inversion_prob:
+            child2 = inversion_mutation(child2)
+        if random.random() < exchange_prob:
+            child1 = exchange_mutation(child1)
+        if random.random() < exchange_prob:
+            child2 = exchange_mutation(child2)
+
+        fitness_child1 = total_distance(child1, distance_lookup)
+        fitness_child2 = total_distance(child2, distance_lookup)
+
+        if fitness_child1 < fitness_child2:
+            new_population.append(child1)
+            new_population.append(parent2)
+        else:
+            new_population.append(child2)
+            new_population.append(parent1)
+
+    return new_population
+
+def genetic_algorithm(distance_matrix, pop_size, tournament_size, crossover_prob, inversion_prob, exchange_prob, num_generations, distance_lookup):
     population = initialize_population(pop_size, len(distance_matrix))
     
     for generation in range(num_generations):
-        new_population = []
-
-        for _ in range(pop_size // 2):
-            parent1 = tournament_selection(population, distance_matrix, tournament_size)
-            parent2 = tournament_selection(population, distance_matrix, tournament_size)
-
-            # Crossover
-            if random.random() < crossover_prob:
-                child1 = pmx_crossover(parent1, parent2)
-                child2 = pmx_crossover(parent2, parent1)
-            else:
-                child1, child2 = parent1[:], parent2[:]
-
-            # Mutation
-            if random.random() < inversion_prob:
-                child1 = inversion_mutation(child1)
-            if random.random() < inversion_prob:
-                child2 = inversion_mutation(child2)
-            if random.random() < exchange_prob:
-                child1 = exchange_mutation(child1)
-            if random.random() < exchange_prob:
-                child2 = exchange_mutation(child2)
-
-            new_population.extend([child1, child2])
-
-        # Replace the old population with the new one
+        new_population = generate_population_and_evaluate(population, distance_lookup, tournament_size)
         population = new_population
 
-    # Find the best individual in the final population
-    best_route = min(population, key=lambda x: total_distance(x, distance_matrix))
-    best_distance = total_distance(best_route, distance_matrix)
+    best_route = min(population, key=lambda x: total_distance(x, distance_lookup))
+    best_distance = total_distance(best_route, distance_lookup)
 
     return best_route, best_distance
 
-def genetic_algorithm_parallel(distance_matrix, pop_size, tournament_size, crossover_prob, inversion_prob, exchange_prob, num_generations):
-    population = initialize_population(pop_size, len(distance_matrix))
-    
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        for generation in range(num_generations):
-            new_population = []
-
-            for _ in range(pop_size // 2):
-                parent1 = tournament_selection(population, distance_matrix, tournament_size)
-                parent2 = tournament_selection(population, distance_matrix, tournament_size)
-
-                # Crossover
-                if random.random() < crossover_prob:
-                    child1 = pmx_crossover(parent1, parent2)
-                    child2 = pmx_crossover(parent2, parent1)
-                else:
-                    child1, child2 = parent1[:], parent2[:]
-
-                # Mutation
-                if random.random() < inversion_prob:
-                    child1 = inversion_mutation(child1)
-                if random.random() < inversion_prob:
-                    child2 = inversion_mutation(child2)
-                if random.random() < exchange_prob:
-                    child1 = exchange_mutation(child1)
-                if random.random() < exchange_prob:
-                    child2 = exchange_mutation(child2)
-
-                new_population.extend([child1, child2])
-
-            # Replace the old population with the new one
-            population = new_population
-
-            # Evaluate fitness in parallel
-            fitness_values = list(executor.map(evaluate_route, population, [distance_matrix] * len(population)))
-
-            # Update the population based on fitness
-            population = [pop for _, pop in sorted(zip(fitness_values, population))]
-
-    # Find the best individual in the final population
-    best_route = population[0]
-    best_distance = total_distance(best_route, distance_matrix)
-
-    return best_route, best_distance
-
-# Example usage
+# Example usage:
 file_path = 'berlin52.txt'
-symmetric_matrix_np = np.array(make_symmetric(load_triangular_matrix(file_path)))
+symmetric_matrix = make_symmetric(load_triangular_matrix(file_path))
+
+# Create the distance lookup table
+distance_lookup = create_distance_lookup(symmetric_matrix)
 
 # Adjusted parameters
 pop_size = 1500
@@ -171,12 +143,8 @@ inversion_prob = 0.1
 exchange_prob = 0.1
 num_generations = 150
 
-best_route, best_distance = genetic_algorithm(symmetric_matrix_np, pop_size, tournament_size, crossover_prob, inversion_prob, exchange_prob, num_generations)
+best_route, best_distance = genetic_algorithm(symmetric_matrix, pop_size, tournament_size, crossover_prob, inversion_prob, exchange_prob, num_generations, distance_lookup=distance_lookup)
 
 print("\nBest Route:")
 print(best_route + [best_route[0]])
 print("\nBest Distance:", best_distance)
-
-
-print("\nDistance matrix: ")
-print(symmetric_matrix_np)
